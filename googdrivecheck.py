20481e5b889aab18674fcd3766eb7ec21c5fec62
jsrozner <jsrozner@gmail.com>
2019-03-07 19:42:39 -0600

    ongoing work; cleaned up post file print

--- googdrivecheck.py
+++ googdrivecheck.py
@@ -1,41 +1,148 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+from pydrive.files import GoogleDriveFile
 
 from pprint import pprint as pp
 from typing import List
 
 import pickle
 
+intense_debug = True
 
-# Util function
+rootdirs = ["My Drive", "My MacBook Air"]
+orphan_prefix = "0_orphan"
+
+# Util functions
 def is_folder(file):
     return file['mimeType'] == "application/vnd.google-apps.folder"
 
+def is_root_folder(file):
+    return file['title'] in rootdirs and len(file['parents']) == 0
+
+# Fetches the first parent's ID.
+# Returns None if no parent
+# Prints notice if more than one parent
+def get_parent(file):
+    parent_array = file['parents']
+    if len(parent_array) == 0:
+        return None
+
+    if(len(parent_array)) > 1:
+        print("** more than one parent")
+
+    return parent_array[0]['id']
+
+
+class FileProperties:
+    @classmethod
+    def get_default_properties(cls):
+        default_dict = {
+            "shared" : False,
+            "spaces_photo" : False,
+            "spaces_app" : False,
+            "trashed" : False,
+            "multi_owners" : False,
+            "non_auth_user_file" : False,
+            "has_more_than_one_permission" : False,
+            "has_non_user_permission" : False,
+            "has_link_sharing" : False
+
+        }
+        return default_dict
+
+class TrackedFile:
+    def __init__(self, file, properties_dictionary: dict):
+        self.file = file
+        self.props = properties_dictionary
+
+        if properties_dictionary['shared']:
+            self._fetch_sharing_metadata()
+
+    def __repr__(self):
+        return self.file['title'] + "\n" + self.props.__repr__()
+
+    def _fetch_sharing_metadata(self):
+        file = self.file
+        is_shared = self.props['shared']
+        file.FetchMetadata(fetch_all=True)
+
+        has_more_than_one_permission = False   # This is not particularly useful
+        has_non_user_permission = False        # This in general will match link_sharing, except in rare cases
+        has_link_sharing = False
+
+        try:
+            if len(file['permissions']) > 1:
+                print("more than one permission ID")
+                self.props['has_more_than_one_permission'] = True
+
+            for permission in file['permissions']:
+                permission_type = permission['type']
+                if permission_type != "user":
+                    print("non user permission type: %s" % permission_type)
+                    self.props['has_non_user_permission'] = True
+                    if permission_type == "anyone":
+                        print("link sharing enabled")
+                        self.props['has_link_sharing'] = True
+
+        except:
+            pp(file)
+
+        # Some verifications:
+        if has_non_user_permission:
+            assert(has_more_than_one_permission and is_shared)
+
+        # Check that it's shared if any of the three conditions are met
+        if has_more_than_one_permission or has_non_user_permission or has_link_sharing:
+            assert is_shared
+
+        # If link sharing, then it has multiple permissions and one is non sharing
+        if has_link_sharing:
+            assert(has_more_than_one_permission and has_non_user_permission and is_shared)
+
+# Represents an individual folder (a file type) - holds
 class Folder:
     def __init__(self, file_id, parent=None, name=""):
         # These two must always be set
         self.id = file_id
         self.num_children = 0
 
-        # These two might be set later
+        # These may be set later
         self.name = name
-        self.parent = parent
+        self.parent = parent    # Of type folder
+        self.children = []
 
+        self.is_orphan = False
+        self.is_root = False
+        self.seen = False       # Set to true when name is parsed; Indicates whether this folder has been
+                                # explicitly seen by metadata / get() fetch
+
+        # Set during post-processing
+        self.metadata_lookup_failed = False
         # This is set after all files are parsed
         self.full_path = None
+        self.all_children_count = 0   # count of all files and subfolders
 
     # Name and parent may be set once the folder is discovered
-    def set_name_and_parent(self, name, parent):
+    def initialize_from_file(self, file, parent):
         if self.name != "":
             print("error: name is already set")
         if self.parent is not None:
             print("error: parent is already set")
 
-        self.name = name
+        self.name = file['title']
+        self.seen = True
         self.parent = parent
+
+        is_root = is_root_folder(file)
+        if is_root: self.is_root = True
+        else: self.is_orphan = True
+
 
     def increment_child_count(self):
         self.num_children += 1
+
+    def add_child(self, child):
+        self.children.append(child)
 
     def set_full_path(self, path):
         if self.full_path is not None:
@@ -47,15 +154,15 @@ class Folder:
     def __repr__(self):
         return " ".join("{}={!r}".format(k, v) for k, v in self.__dict__.items())
 
-
 # A dictionary of Folders
 # id => Folder
+# A dictionary with a few new features. Not implemented in the prettiest way
 class FolderTracker:
     def __init__(self):
         self.data = dict()
 
     # Look up without creation of a new folder if one doesn't exist
-    def static_folder_lookup(self, folder_id):
+    def static_folder_lookup(self, folder_id) -> Folder:
         look_up_result = self.data.get(folder_id)
         if not look_up_result:
             print("error not found")
@@ -73,149 +180,166 @@ class FolderTracker:
             self.data[folder_id] = new_folder
             return new_folder
 
+    # Records the file. 1) Logs in enclosing parent folder; 2) If this is a folder,
+    # then creates folder for this file
     def log_item(self, file):
         file_id = file['id']
-        name = file['title']
-        parents = file['parents']
-        if len(parents) == 0:
-            print("no parents - potentially a shared orphan file")
-            return
-
-        parent = parents[0]['id']
-        if(len(parents)) > 1:
-            print("more than one parent")
         is_a_folder = is_folder(file)
+        parent = get_parent(file)
+        parent_folder = None
+
+        if parent is not None:
+            # Log this file in its enclosing folder, too
+            parent_folder = self._get_folder_or_initialize(parent)
+            parent_folder.increment_child_count()
 
         if is_a_folder:
             # Record the folder info
             folder = self._get_folder_or_initialize(file_id)
-            folder.set_name_and_parent(name, parent)
+            folder.initialize_from_file(file, parent=parent_folder)
+            # only log a child if the child is itself a folder
+            if parent is not None:
+                parent_folder.add_child(folder)
 
-        # Log this file in its enclosing folder, too
-        enclosing_folder = self._get_folder_or_initialize(parent)
-        enclosing_folder.increment_child_count()
 
-
+    # Recursively lookup fullpaths through the folder tree
     def get_full_path(self, folder: Folder):
-        #print("getting path for folder in get_full_path")
-        #pp(folder)
         # Quick fail if we've already done this node
         if folder.full_path is not None:
-            #print("already had full path for folder")
             return folder.full_path
+
+        # If we never encountered this folder before, then we walk up the folder tree doing lookups
+        # In a single function call, this should populate the current folder's parent
+        if not folder.seen and not folder.metadata_lookup_failed:
+            print("fetching metadata for %s" % folder.id)
+            try:
+                # We try to fetch the data for this file
+                file_to_fetch = drive.CreateFile({'id': folder.id})
+                print("fetched metadata for %s" % file_to_fetch['title'])
+
+                parent_id = get_parent(file_to_fetch)
+                parent_folder = None
+                if parent_id is not None:
+                    parent_folder = Folder(parent_id)
+
+                # We also create a parent folder for this file
+                folder.initialize_from_file(file_to_fetch, parent=parent_folder)
+
+            except(Exception):
+                print("error trying to fetch folder")
+                print(str(Exception))
+                pp(file_to_fetch)
+                pp(folder)
+                folder.metadata_lookup_failed = True
+
+                # This file is not "see-able". Fall back to a "...". The folder.name here should not be populated.
+                full_path = ".../" + folder.name
+                folder.set_full_path(full_path)
+                return full_path
 
         # Base case: no more parents
+        # If we are here, there are no more parents because it is root, orphaned, or error
+        full_path = ""
         if folder.parent is None:
-            #print("%s is a root node" % folder.name)
+            if folder.is_root:
+                full_path = folder.name
+            elif folder.is_orphan:
+                full_path = orphan_prefix + "/" + folder.name
+            # Else it's a metadata lookup failure
+            # We should never get here, since metadata lookup failures already set the fullpath
+            else:
+                print("*** ERROR *** no parent (and not root or orphan) for id: %s" % folder.id)
 
-            if folder.name == "":
-                #print("this folder was never seen. Setting name to ??")
-                folder.set_full_path("??")
-            return folder.full_path
-
-        # Otherwise, we go fetch the parent
-        #print("looking up the parent")
-        parent = self.static_folder_lookup(folder.parent)
-        if parent is None:
-            #print("we never saw this parent")
-            # We never actually saw this folder (started at non root level)
-            full_path = ".../" + folder.name
-            #print("path is: %s" % full_path)
             folder.set_full_path(full_path)
             return full_path
 
-        #print("parent is not none: recursing")
-        full_path = self.get_full_path(parent) + "/" + folder.name
+        # Otherwise recursively fetch
+        full_path = self.get_full_path(folder.parent) + "/" + folder.name
         folder.set_full_path(full_path)
         return full_path
 
 
     def populate_all_paths(self):
         for folder in self.data.values():
-            #print("*** getting path for folder")
-            #pp(folder)
             if folder.full_path is None:
                 self.get_full_path(folder)
 
 def print_set(set_name, file_set: List):
     print("** Printing set: %s" % set_name)
     for file in file_set:
-        parent_folder_id = file['parents'][0]['id']
-        parent_folder = all_folders.static_folder_lookup(parent_folder_id)
-        parent_folder_path = all_folders.get_full_path(parent_folder)
-        file['fullpath'] = parent_folder_path + "/" + file['title']
+        parent_folder_id = get_parent(file)
+        if parent_folder_id is None:
+            file['fullpath'] = orphan_prefix + "/" + file['title']
+        else:
+            parent_folder = all_folders.static_folder_lookup(parent_folder_id)
+            parent_folder_path = all_folders.get_full_path(parent_folder)
+            file['fullpath'] = parent_folder_path + "/" + file['title']
 
     file_set.sort(key=lambda x: x['fullpath'])
     pp([x['fullpath'] for x in file_set])
 
 
 def check_file_sharing(file):
-    if "photos" in file["spaces"]:
-        spaces_photos_set.append(file)
-    elif "appDataFolder" in file["spaces"]:
-        spaces_app_data_folder_set.append(file)
-
-    if file['labels']['trashed']:
-        trash_set.append(file)
-
-
-    has_a_problem = False
-    shared = False
-    more_than_one_permission = False
-    link_sharing = False
-
-    # Special errors
-    if len(file['owners']) > 1:
-        has_a_problem = True
-        more_than_one_owner_set.append(file)
-
-    if not "Josh Rozner" in file['ownerNames']:
-        not_owned_by_me_set.append(file)
-        has_a_problem = True
-
-    # End special errors
-
-    # Sharing info
-    """
-    if len(file['permissionIds']) > 1:
-        print("more than one permission ID")
-        more_than_one_permission_set.append(file)
-        has_a_problem = True
-
-    if "anyoneWithLink" in file['permissionsIds']:
-        print("link sharing enabled")
-        link_sharing_set.append(file)
-        has_a_problem = True
-    
-    try:
-        if len(file['permissions']) > 1:
-            print("more than one permission ID")
-            more_than_one_permission_set.append(file)
-            more_than_one_permission = True
-
-        for permission in file['permissions']:
-            permission_type = permission['value']['type']
-            if permission_type != "user":
-                print("non user permission type: %s" % permission_type)
-                link_sharing = True
-                link_sharing_set.append(file)
-    except:
-        pp(file)
-    # Some verifications:
-    if bool(shared) ^ bool(more_than_one_permission):
-        print("inconcistency in shared and permission count")
-
-    if bool(link_sharing) ^ bool(shared):
-        print("inconsistency in shared and link sharing")
-    """
+    file_id = file['id']
+    properties_dict = FileProperties.get_default_properties()
 
     if file['shared']:
-        sharing_set.append(file)
-        shared = True
+        properties_dict['shared'] = True
         print("file is shared")
 
+    if "photos" in file["spaces"]:
+        properties_dict['spaces_photo'] = True
+    elif "appDataFolder" in file["spaces"]:
+        properties_dict['spaces_app'] = True
 
+    if file['labels']['trashed']:
+        properties_dict['trashed'] = True
 
+    if len(file['owners']) > 1:
+        properties_dict['multi_owners'] = True
+
+    if not "Josh Rozner" in file['ownerNames']:
+        properties_dict['non_auth_user_file'] = True
+
+    if any(properties_dict):
+        tracked_files[file_id] = TrackedFile(file, properties_dict)
+
+def run_with_recursive_look_up(starting_id):
+    #todo_stack = [""]
+    todo_stack = [starting_id]
+    total_files = 0
+    while len(todo_stack) > 0:
+        next_parent = todo_stack.pop()
+        q_string = "'" + next_parent + "'" + " in parents and trashed=false"
+        query = {'maxResults': 1000, 'q': q_string}
+        for file_list in drive.ListFile(query):
+            total_files += len(file_list)
+            print(total_files)
+            for f in file_list:
+                if intense_debug:
+                    all_file_set.append(f)
+                all_folders.log_item(f)
+                check_file_sharing(f)
+                if is_folder(f):
+                    todo_stack.append(f['id'])
+
+    return total_files
+
+def run_with_query(query=""):
+    if query == "":
+        q_string = "trashed=false"
+        query = {'maxResults': 1000, 'q': q_string}
+
+    total_files = 0
+    for file_list in drive.ListFile(query):
+        total_files += len(file_list)
+        print(total_files)
+        for f in file_list:
+            #all_file_set.append(f)
+            all_folders.log_item(f)
+            check_file_sharing(f)
+
+    return total_files
 
 if __name__ == "__main__":
     # Auth login (see also settings.yaml)
@@ -224,79 +348,37 @@ if __name__ == "__main__":
     drive = GoogleDrive(gauth)
 
     # Data accumulation
-    all_folders = FolderTracker()
+    all_folders = FolderTracker()   # Accumulates all folders during run
+    tracked_files = dict()           # Accumulates files of interest during run
 
-    # tracking sets
-    spaces_photos_set = []
-    spaces_app_data_folder_set = []
+    all_file_set = []       # Only for intense debugging; not generally used
 
-    not_owned_by_me_set = []
-    trash_set = []
-    more_than_one_owner_set = []
-
-    #link_sharing_set = []
-    #more_than_one_permission_set = []
-    sharing_set = []
-
-    all_file_set = []
-
-
-    total_files = 0
-    # Note: already verified this does not have duplicates
     #todo: make sure owner is josh, not in trash
-    """
-    todo_stack = ["0B4aSdoErkE3vfnp2Mi0zYTBVVWhETHBkalA3eUJkN3JCNXlLaUtRSGxuckRKZ05vNTBRdVk"]
+    # Note: already verified this does not have duplicates
 
-    while len(todo_stack) > 0:
-        next_parent = todo_stack.pop()
-        q_string = "'" + next_parent + "'" + " in parents and trashed=false"
-        query = {'maxResults': 1000, 'q': q_string}
-        #simple_query = {'maxResults': 1000}
-        for file_list in drive.ListFile(query):
-            #pp(file_list)
-            total_files += len(file_list)
-            print(total_files)
-            for f in file_list:
-                #all_file_set.append(f)
-                all_folders.log_item(f)
-                check_file_sharing(f)
-                if is_folder(f):
-                    todo_stack.append(f['id'])
-    """
-    q_string = "trashed=false"
-    query = {'maxResults': 1000, 'q': q_string}
-    for file_list in drive.ListFile(query):
-        total_files += len(file_list)
-        print(total_files)
-        for f in file_list:
-            #all_file_set.append(f)
-            all_folders.log_item(f)
-            check_file_sharing(f)
-    print(total_files)
+    # ******* This is the main run *********
+    run_with_recursive_look_up("0B4aSdoErkE3vTHRTdzRzOTBIR3M") # "active"
+    #run_with_query()
+
+    # Post processing
     all_folders.populate_all_paths()
-    #pp(all_folders)
+    if intense_debug:
+        print_set("All files", all_file_set)
 
+    pp(tracked_files)
 
-    #print_set("All files", all_file_set)
-    print_set("Shared files", sharing_set)
-    #print_set("Link sharing", link_sharing_set)
-    #print_set("More than one user has access", more_than_one_permission_set)
-
-    print_set("Not owned by me", not_owned_by_me_set)
-    print_set("In trash", trash_set)
-    print_set("More than one owner", more_than_one_owner_set)
-    print_set("Spaces: photos", spaces_photos_set)
-    print_set("Spaces: app data", spaces_app_data_folder_set)
-
-    pickle_file = open("pickleoutput.db", "wb")
-    pickle.dump(sharing_set, pickle_file)
-    pickle.dump(not_owned_by_me_set, pickle_file)
-    pickle.dump(trash_set, pickle_file)
-    pickle.dump(more_than_one_owner_set, pickle_file)
-    pickle.dump(spaces_photos_set, pickle_file)
-    pickle.dump(spaces_app_data_folder_set, pickle_file)
-    pickle.dump(all_folders, pickle_file)
-    pickle_file.close()
+    should_write = True
+    if should_write:
+        pickle_file = open("pickleoutput.db", "wb")
+        """pickle.dump(sharing_set, pickle_file)
+        pickle.dump(not_owned_by_me_set, pickle_file)
+        pickle.dump(trash_set, pickle_file)
+        pickle.dump(more_than_one_owner_set, pickle_file)
+        pickle.dump(spaces_photos_set, pickle_file)
+        pickle.dump(spaces_app_data_folder_set, pickle_file)"""
+        pickle.dump(all_folders, pickle_file)
+        pickle.dump(tracked_files, pickle_file)
+        pickle_file.close()
 
 
     # todo: see where all the massive file set is hidden
