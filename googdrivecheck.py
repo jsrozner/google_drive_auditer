b2cc2b1d27d9c0d799e7d4881123b9e4ee7658e5
jsrozner <jsrozner@gmail.com>
2019-03-08 20:05:39 -0600

    added properties, cleaned up

--- googdrivecheck.py
+++ googdrivecheck.py
@@ -1,11 +1,21 @@
+from __future__ import annotations
 from pprint import pprint as pp
-from typing import List
+from typing import List, Optional
 import pickle
 
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
 
-from typing import Optional
+def lazy_property_folder_metadata(fn):
+    """Decorator for lazily fetching certain metadata"""
+    @property
+    def _lazy_property(self: Folder):
+        if not self._seen:
+            self._do_lookup_from_drive()
+        if self._metadata_lookup_failed:
+            print("Metadata lookup unsuccessful. Default value returned for %s" + fn.__repr__)
+        return fn(self)
+    return _lazy_property
 
 # Todo
 intense_debug = False
@@ -15,6 +25,7 @@ debug = False
 # Drive defines "My Drive" as root, but backed up computers are not captured.
 rootdirs = ["My Drive", "My MacBook Air"]
 orphan_prefix = "0_orphan"
+name_for_non_seeable_folders = "no_name"
 
 # Util functions
 def is_folder(file):
@@ -133,50 +144,69 @@ class Folder:
     def __repr__(self):
         return " ".join("{}={!r}".format(k, v) for k, v in self.__dict__.items())
 
-    def __init__(self, file_id, parent=None, name=""):
+    def __init__(self, file_id):
         # These two are always set and safe to read
         self.id = file_id
-        self.num_direct_children = 0
+        self.num_direct_children = 0    # This is incremented during processing. Early fetches may be wrong
+        self.child_folders = []         # Same as above
 
-        # These may be set later
-        self.parent = parent    # Of type folder
-        self.children = []
-
-        self.is_orphan = False
-        self.is_root = False
-        self.seen = False       # Set to true when name is parsed; Indicates whether this folder has been
+        # Whether file has been explicitly processed
+        self._seen = False      # Set to true when name is parsed; Indicates whether this folder has been
                                 # explicitly seen by metadata / get() fetch
-        self.depth = 0          # Todo: populate depth
+        self._metadata_lookup_failed = False
 
-        # Updated during post-processing
-        self.metadata_lookup_failed = False
-        # This is set after all files are parsed
-        self.all_children_count = 0   # count of all files and subfolders
+        # Properties (looked up lazily and protected by property method). Default values below.
+        # Guarded by self._seen
+        self._is_orphan = False  # Could be wrong if lookup failed
+        self._is_root = False    # Could be wrong if lookup failed
+        self._full_path = None   # Generally set during post-processing. Lazily fetched
+        self._name = ""          # Set by file lookup. Lazily fetched
+        self._parent = None      # Set by file lookup. Lazily fetched
 
-        # Properties
-        self._full_path = None  # Generally set during post-processing
-        self._name = name
+        # True post processing (last traverse of tree). If these values differ from -1, then have been set
+        self._all_children_count = -1   # count of all individual subchildren (through subdirectories to end)
+        self._depth = -1
 
-    # Name and parent may be set once the folder is discovered
-    def initialize_from_file(self, file, parent):
-        if self.name != "":
-            print("error: name is already set")
-        if self.parent is not None:
-            print("error: parent is already set")
+    # Populates all fields for the folder.
+    def populate_fields_from_file(self, file, parent):
+        if self._seen:
+            raise Exception("Fields have already been populated once")
+        if self._metadata_lookup_failed:
+            raise Exception("Metadata lookup failed. Invalid call to populate fields")
+        self._seen = True
 
-        self.name = file['title']
-        self.seen = True
-        self.parent = parent
+        self._name = file['title']
+        self._parent = parent
 
-        is_root = is_root_folder(file)
-        if is_root: self.is_root = True
-        else: self.is_orphan = True
+        if self._parent is None:
+            if is_root_folder(file): self._is_root = True
+            else: self._is_orphan = True
 
-    def increment_child_count(self):
-        self.num_direct_children += 1
+    # Do a google drive lookup and fill the normal fields
+    def _do_lookup_from_drive(self):
+        if self._seen or self._metadata_lookup_failed:
+            return
 
-    def add_child(self, child):
-        self.children.append(child)
+        file_to_fetch = None
+        try:
+            # We try to fetch the data for this file
+            file_to_fetch = drive.CreateFile({'id': self.id})
+            print("fetched metadata for %s" % file_to_fetch['title'])
+
+            parent_id = get_parent(file_to_fetch)
+            parent_folder = None
+            if parent_id is not None:
+                parent_folder = Folder(parent_id)
+
+            # We also create a parent folder for this file
+            self.populate_fields_from_file(file_to_fetch, parent=parent_folder)
+        except Exception:
+            print("error trying to fetch folder %s" % self.id)
+            print(str(Exception))
+            pp(file_to_fetch)
+            pp(self)
+            self._metadata_lookup_failed = True
+            self._name = name_for_non_seeable_folders
 
     # Recursively lookup fullpaths through the folder tree
     @property
@@ -187,59 +217,72 @@ class Folder:
 
         # If we never encountered this folder before, then we walk up the folder tree doing lookups
         # In a single function call, this should populate the current folder's parent
-        if not self.seen and not self.metadata_lookup_failed:
-            print("fetching metadata for %s" % self.id)
-            file_to_fetch = None
-            try:
-                # We try to fetch the data for this file
-                file_to_fetch = drive.CreateFile({'id': self.id})
-                print("fetched metadata for %s" % file_to_fetch['title'])
+        if not self._seen and not self._metadata_lookup_failed:
+            self._do_lookup_from_drive()
 
-                parent_id = get_parent(file_to_fetch)
-                parent_folder = None
-                if parent_id is not None:
-                    parent_folder = Folder(parent_id)
+        if self._metadata_lookup_failed:
+            # This file is not "see-able". Fall back to a "...". The folder.name here should not be populated.
+            self._full_path = ".../" + self.name
+            return self._full_path
 
-                # We also create a parent folder for this file
-                self.initialize_from_file(file_to_fetch, parent=parent_folder)
+        # Otherwise, file is seen and metadata was looked up. Now we populate the full_path
+        if self.parent is not None:
+            self._full_path = self.parent.full_path + "/" + self.name
+            return self._full_path
 
-            except Exception:
-                print("error trying to fetch folder")
-                print(str(Exception))
-                pp(file_to_fetch)
-                pp(self)
-                self.metadata_lookup_failed = True
-
-                # This file is not "see-able". Fall back to a "...". The folder.name here should not be populated.
-                full_path = ".../" + self.name
-                self._full_path = full_path
-                return full_path
-
-        # Base case: no more parents
         # If we are here, there are no more parents because it is root, orphaned, or error
-        full_path = ""
-        if self.parent is None:
-            if self.is_root:
-                full_path = self.name
-            elif self.is_orphan:
-                full_path = orphan_prefix + "/" + self.name
-            # Else it's a metadata lookup failure
-            # We should never get here, since metadata lookup failures already set the fullpath
-            else:
-                print("*** ERROR *** no parent (and not root or orphan) for id: %s" % self.id)
+        if self.is_root:
+            self._full_path = self.name
+        elif self.is_orphan:
+            self._full_path = orphan_prefix + "/" + self.name
+        # Else it's a metadata lookup failure
+        # We should never get here, since metadata lookup failures already set the fullpath
+        else:
+            print("*** ERROR *** no parent (and not root or orphan) for id: %s" % self.id)
 
-            self._full_path = full_path
-            return full_path
-
-        # Otherwise recursively fetch
-        self._full_path = self.parent.full_path + "/" + self.name
         return self._full_path
 
-    # todo: make sure name is populated (by fetch)
-    # remove self.seen lookup?
-    @property
+    # Properties that require full metadata
+    @lazy_property_folder_metadata
     def name(self):
         return self._name
+    @lazy_property_folder_metadata
+    def parent(self):
+        return self._parent
+    @lazy_property_folder_metadata
+    def is_orphan(self):
+        return self._is_orphan
+    @lazy_property_folder_metadata
+    def is_root(self):
+        return self._is_root
+
+    # Post processing. Both functions are recursive and iterate through all folders.
+    @property
+    def all_children_count(self):
+        if self._all_children_count > -1:   # already set (only do once)
+            return self._all_children_count
+        self._all_children_count = 0
+
+        # Otherwise, iterate through the child_folders
+        for child in self.child_folders:
+            self._all_children_count += child.get_all_children_count()
+        self._all_children_count += self.num_direct_children
+
+        return self._all_children_count
+
+    @property
+    def depth(self):
+        if self._depth > -1:    # already set (only do once)
+            return self._depth
+
+        depth = 0
+        current_node = self
+        while not current_node.is_orphan and not current_node.is_root:
+            depth += 1
+            current_node = current_node.parent
+        return depth
+
+
 # A dictionary of Folders
 # id => Folder
 # A dictionary with a few new features. Not implemented in the prettiest way
@@ -251,9 +294,7 @@ class FolderTracker:
     def static_folder_lookup(self, folder_id) -> Optional[Folder]:
         look_up_result = self.data.get(folder_id)
         if not look_up_result:
-            print("error not found")
-            return None
-
+            raise Exception("Folder not found in static_folder_lookup: %s" % folder_id)
         return look_up_result
 
     # Look up and initialize
@@ -261,7 +302,7 @@ class FolderTracker:
         if folder_id in self.data:
             return self.data[folder_id]
         else:
-            # Initialize a new folder with zero children, since we've never seen it before
+            # Initialize a new folder with zero child_folders, since we've never seen it before
             new_folder = Folder(folder_id)
             self.data[folder_id] = new_folder
             return new_folder
@@ -277,15 +318,15 @@ class FolderTracker:
         if parent is not None:
             # Log this file in its enclosing folder, too
             parent_folder = self._get_folder_or_initialize(parent)
-            parent_folder.increment_child_count()
+            parent_folder.num_direct_children += 1
 
         if is_a_folder:
             # Record the folder info
             folder = self._get_folder_or_initialize(file_id)
-            folder.initialize_from_file(file, parent=parent_folder)
+            folder.populate_fields_from_file(file, parent=parent_folder)
             # only log a child if the child is itself a folder
             if parent is not None:
-                parent_folder.add_child(folder)
+                parent_folder.child_folders.append(folder)
 
     # Postprocessing: recursively fill the paths for all folders
     def populate_all_paths(self):
@@ -337,6 +378,7 @@ def run_with_recursive_look_up(starting_id):
     #todo_stack = [""]
     todo_stack = [starting_id]
     total_files = 0
+    total_folders = 0
     while len(todo_stack) > 0:
         next_parent = todo_stack.pop()
         q_string = "'" + next_parent + "'" + " in parents and trashed=false"
@@ -351,24 +393,28 @@ def run_with_recursive_look_up(starting_id):
                 check_file_sharing(f)
                 if is_folder(f):
                     todo_stack.append(f['id'])
+                    total_folders += 1
 
-    return total_files
+    print("Parsed %d files\t %d folders" % total_all_files, total_folders)
+    return total_files, total_folders
 
 def run_with_query(query=""):
     if query == "":
         q_string = "trashed=false"
         query = {'maxResults': 1000, 'q': q_string}
 
-    total_files = 0
+    total_all_files = 0
+    total_folders = 0
     for file_list in drive.ListFile(query):
-        total_files += len(file_list)
-        print(total_files)
+        total_all_files += len(file_list)
+        print(total_all_files)
         for f in file_list:
-            #all_file_set.append(f)
+            if is_folder(f): total_folders += 1
             all_folders.log_item(f)
             check_file_sharing(f)
 
-    return total_files
+    print("Parsed %d files\t %d folders" % total_all_files, total_folders)
+    return total_all_files, total_folders
 
 if __name__ == "__main__":
     # Auth login (see also settings.yaml)
--- listfoldersbysize.py
+++ listfoldersbysize.py
@@ -4,16 +4,6 @@ import sys
 from googdrivecheck import Folder, FolderTracker
 from typing import List
 
-def get_all_children_count(folder: Folder):
-    # If this folder is already set, then just return
-    if folder.all_children_count > 0:
-        return folder.all_children_count
-
-    # Otherwise, iterate through the children
-    for f in folder.children:
-        folder.all_children_count += get_all_children_count(f)
-    return folder.all_children_count + folder.num_direct_children
-
 def print_folders(folders: List[Folder]):
     print("** Printing folders:")
     folders.sort(key=lambda x: x.full_path)
@@ -25,16 +15,9 @@ def print_folders(folders: List[Folder]):
 if __name__ == "__main__":
     pf = open(sys.argv[1], "rb")
     #pf = open("./pickleoutput.db")
-    shared = pickle.load(pf)
-    not_owned_by_me = pickle.load(pf)
-    trash_set = pickle.load(pf)
-    more_than_one_owner_set = pickle.load(pf)
-    spaces_photo_set = pickle.load(pf)
-    spaces_app_data_set = pickle.load(pf)
     all_folders: FolderTracker = pickle.load(pf)
-
     folder_list = list(all_folders.data.values())
     for f in folder_list:
-        get_all_children_count(f)
+        f.get_all_children_count()
 
     print_folders(folder_list)
